b4b35a5fa526aa6868020b3839f0fc803836eecd
glerisonlima <glerisonlima@gmail.com>
2019-07-22 00:59:09 -0300

    salvando no github pages

---
+++ precache-manifest.bb3c90d004ff1bcb1a2395f051cf13ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece94170facafcc047df097525f185e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1d38e1ae1f56d66fac",
+    "url": "/static/css/main.c43ca0f9.chunk.css"
+  },
+  {
+    "revision": "f9e599bac12e23b159fc",
+    "url": "/static/js/2.6d25ea15.chunk.js"
+  },
+  {
+    "revision": "9f1d38e1ae1f56d66fac",
+    "url": "/static/js/main.ab9defb7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
